bb6dee8e192c264160e3321aa42e745f3167065c
Salim-54 <ndayisabyesalim54@gmail.com>
2023-06-05 13:05:03 +0200

    intial commit

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,116 @@
+import React, { useState } from "react";
+import { ContactWrapper, Email } from "./ContactElements";
+import { MdContentCopy } from "react-icons/md";
+import { IconButton, Tooltip } from "@mui/material";
+import Zoom from '@mui/material/Zoom';
+
+import ScrollAnimation from "react-animate-on-scroll";
+function Contact() {
+  const [showTooltip, setShowTooltip] = useState(false);
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText("ndayisabyesalim54@gmail.com");
+    setShowTooltip(true);
+    setTimeout(() => {
+      setShowTooltip(false);
+    }, 700);
+  };
+
+  return (
+    <ContactWrapper id="contact">
+      <div className="Container">
+        <div className="SectionTitle">Get In Touch</div>
+        <ScrollAnimation animateIn="fadeIn">
+          <div className="BigCard">
+            <Email>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  columnGap: "20px",
+                  rowGap: "10px",
+                  flexWrap: "wrap",
+                  justifyContent: "center",
+                }}
+              >
+                <span>ndayisabyesalim54@gmail.com</span>
+                <Tooltip
+                  PopperProps={{
+                    disablePortal: true,
+                  }}
+                  open={showTooltip}
+                  onClose={() => setShowTooltip(false)}
+                  title="Copied!"
+                  TransitionComponent={Zoom}
+                  disableFocusListener
+                  disableHoverListener
+                  disableTouchListener
+                  placement="bottom"
+                >
+                  <IconButton onClick={copyToClipboard}>
+                    <MdContentCopy
+                      size={25}
+                      style={{ cursor: "pointer", color: "#151418" }}
+                    />
+                  </IconButton>
+                </Tooltip>
+              </div>
+              <a
+                className="btn PrimaryBtn btn-shadow"
+                href="mailto:ndayisabyesalim54@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Send Email
+              </a>
+            </Email>
+            <br />
+            <Email>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  columnGap: "20px",
+                  rowGap: "10px",
+                  flexWrap: "wrap",
+                  justifyContent: "center",
+                }}
+              >
+                <span>+250 789 393 544</span>
+                <Tooltip
+                  PopperProps={{
+                    disablePortal: true,
+                  }}
+                  open={showTooltip}
+                  onClose={() => setShowTooltip(false)}
+                  title="Copied!"
+                  TransitionComponent={Zoom}
+                  disableFocusListener
+                  disableHoverListener
+                  disableTouchListener
+                  placement="bottom"
+                >
+                  <IconButton onClick={copyToClipboard}>
+                    <MdContentCopy
+                      size={25}
+                      style={{ cursor: "pointer", color: "#151418" }}
+                    />
+                  </IconButton>
+                </Tooltip>
+              </div>
+              <a
+                className="btn PrimaryBtn btn-shadow"
+                href="tel:0789393544"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Call me
+              </a>
+            </Email>
+          </div>
+        </ScrollAnimation>
+      </div>
+    </ContactWrapper>
+  );
+}
+
+export default Contact;
---
+++ src/data/ProjectData.js
@@ -0,0 +1,145 @@
+// --------------------------------------- Project List
+export const ProjectList = [
+  {
+    img: "/r-yes.png",
+    title: "R-YES e-Recruitment",
+    description:
+      "A popular job seeking platform for agriculture and food processing experts based in East Africa",
+    tech_stack: ["Flutter", "React", "PHP"],
+    github_url: "https://play.google.com/store/apps/details?id=com.r_yes",
+    demo_url: "https://r-yes.com/",
+  },
+  {
+    img: "/ishema.png",
+    title: "RD-Tech smart inventory system",
+    description:
+      "An app where users can purchase online therapy sessions and chat with their therapists.",
+    tech_stack: ["ReactJS", "NodeJs", "MongoDB", "SocketIo", "NFC"],
+    github_url: "#",
+    demo_url: "#",
+  },
+  {
+    img: "/tuza.png",
+    title: "Tuza Pay",
+    description: "Tuzapay is a blockchain based neobank",
+    tech_stack: ["PHP", "MySQL"],
+    github_url: "",
+    demo_url: "http://www.tuzapay.com/",
+  },
+  {
+    img: "/east-flix.png",
+    title: "EastFlix",
+    description: "A popular and dominant entertainment platform in Rwanda.",
+    tech_stack: ["Flutter", "PHP", "MySQL"],
+    github_url: "#",
+    demo_url: "https://eastflix.tv/",
+  },
+  {
+    img: "/mmd.png",
+    title: "Megga meds",
+    description: "A medical ATM system.",
+    tech_stack: ["Flutter", "Embedded  C", "Python"],
+    github_url: "#",
+    demo_url: "#",
+  },
+  {
+    img: "/smolleys.png",
+    title: "Smolleys",
+    description:
+      "Digital bank for kids and a perfect platform for money and entrepreneurial literacy",
+    tech_stack: ["AI", "Flutter", "NodeJs", "ReactJs", "Postgres"],
+    github_url:
+      "https://play.google.com/store/apps/details?id=com.edhee.smolleys",
+    demo_url: "https://smolleys.com/",
+  },
+  {
+    img: "https://play-lh.googleusercontent.com/oMA2ueUhaOx8Pvm933AfhzXwmGBOKjgXjx-HcuqKXm7GtkBcMiiznJB_wTUheyTaABU=w240-h480-rw",
+    title: "IGAJU",
+    description:
+      "An app to scan children for any autism symptom it is a project by AIMS.",
+    tech_stack: ["Dart", "Flutter", "Nodejs", "Postgres"],
+    github_url: "https://play.google.com/store/apps/details?id=rw.igaju.autism",
+    demo_url: "",
+  },
+];
+
+// --------------------------------------- Skills 
+
+export const stackList = [
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original.svg",
+    name: "HTML",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original.svg",
+    name: "CSS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+    name: "JavaScript",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "ReactJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-original.svg",
+    name: "Bootstrap",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flutter/flutter-original.svg",
+    name: "Flutter",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/git/git-original.svg",
+    name: "Git",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original.svg",
+    name: "React Native",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original.svg",
+    name: "NodeJS",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/firebase/firebase-plain.svg",
+    name: "Firebase",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original.svg",
+    name: "MongoDB",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+    name: "Python",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/flask/flask-original.svg",
+    name: "Flask",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/c/c-original.svg",
+    name: "C",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+    name: "Java",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original.svg",
+    name: "MySQL",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/materialui/materialui-original.svg",
+    name: "Material UI",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/figma/figma-original.svg",
+    name: "Figma",
+  },
+  {
+    img: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vscode/vscode-original.svg",
+    name: "VS Code",
+  },
+];
